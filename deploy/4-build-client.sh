30500cddbe132cf8ac851e694f9e270f2e24a8b4
Harrison Balogh <harrisonbalogh@gmail.com>
2022-09-01 15:21:51 -0700

    client: session manager lib uplift

--- deploy/4-build-client.sh
+++ deploy/4-build-client.sh
@@ -1,6 +1,7 @@
  #!/bin/bash
 
 cd ./Client
-
+path=$HTTP_DIR/projects/$2
+mkdir -p $path
 # Rebuild client to include release changes
-(npm install; npm run build --production; cp -a ./dist/. $HTTP_DIR)
+(npm install; npm run build --production; cp -a ./dist/. $path)
